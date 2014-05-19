4d8989b67d688f28398b0857c7b80ea6ab5e34a7
Ben Taylor <bewt85@gmail.com>
2014-05-19 16:19:35 +0000

    Frontend now returns a message from the backend

--- flask_frontend/frontend.py
+++ flask_frontend/frontend.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+import requests
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    r = requests.get('http://localhost:5001/')
+    backend_message = r.json()['message']
+    frontend_message = "Hello world from the frontend and %s" % backend_message 
+    return frontend_message 
 
 if __name__ == '__main__':
     app.run(port=5000)
