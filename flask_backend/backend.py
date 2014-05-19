7cc62c8758d9b246fe1a68c609cc777ebfb9bbd8
Ben Taylor <bewt85@gmail.com>
2014-05-19 16:06:15 +0000

    Backend now returns json

--- flask_backend/backend.py
+++ flask_backend/backend.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, I am the backend!' 
+    message={'message': 'Hello, I am the backend!'}
+    return jsonify(**message)
 
 if __name__ == '__main__':
     app.run(port=5001)
