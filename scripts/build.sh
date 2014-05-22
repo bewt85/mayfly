4c40c96aabf6fbb0dbcbb93ae8d89328b5f89484
Ben Taylor <bewt85@gmail.com>
2014-05-22 23:34:51 +0000

    Update scripts for haproxy
    Scripts now include build and run for haproxy and configure_haproxy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker build -t bewt85/frontend      frontend/
-docker build -t bewt85/backend       backend/
-docker build -t bewt85/dnsmasq       dnsmasq/
-docker build -t bewt85/configure_dns configure_dns/
+docker build -t bewt85/frontend          frontend/
+docker build -t bewt85/backend           backend/
+docker build -t bewt85/dnsmasq           dnsmasq/
+docker build -t bewt85/configure_dns     configure_dns/
+docker build -t bewt85/haproxy           haproxy/
+docker build -t bewt85/configure_haproxy configure_haproxy/
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,10 +2,13 @@
 
 HOST_IP=`ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -d: -f2`
 
-docker run -d --name dnsmasq -t bewt85/dnsmasq
+docker run -d --name dnsmasq   -t                           bewt85/dnsmasq
 
 DNS_IP=`docker inspect dnsmasq | awk -F '"' '/IPAddress/ {print $4}'`
 
-docker run -i --rm --volumes-from dnsmasq bewt85/configure_dns update frontend.service "$HOST_IP" backend.service "$HOST_IP" 
-docker run -d --name backend -t -p 5001:8080 --dns $DNS_IP bewt85/backend
+docker run -d --name backend  -t -p 5001:8080 --dns $DNS_IP bewt85/backend
 docker run -d --name frontend -t -p 5000:8080 --dns $DNS_IP bewt85/frontend
+docker run -d --name haproxy  -t -p 8080:80   --dns $DNS_IP bewt85/haproxy
+
+docker run -i --rm --volumes-from dnsmasq bewt85/configure_dns update frontend.service "$HOST_IP" backend.service "$HOST_IP" 
+# docker run -i --rm --volumes-from haproxy -t bewt85/configure_haproxy
