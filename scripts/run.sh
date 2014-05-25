9ee3217bc3346d5c28074e4da4d0ee3d9dddefd9
Ben Taylor <bewt85@gmail.com>
2014-05-25 00:29:30 +0000

    HAProxy now listening on port 80

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,7 +8,7 @@ DNS_IP=`docker inspect dnsmasq | awk -F '"' '/IPAddress/ {print $4}'`
 
 docker run -d --name backend  -t -p 5001:8080 --dns $DNS_IP bewt85/backend
 docker run -d --name frontend -t -p 5000:8080 --dns $DNS_IP bewt85/frontend
-docker run -d --name haproxy  -t -p 8080:80   --dns $DNS_IP bewt85/haproxy
+docker run -d --name haproxy  -t -p 80:80     --dns $DNS_IP bewt85/haproxy
 
 docker run -i --rm --volumes-from dnsmasq bewt85/configure_dns update frontend.service "$HOST_IP" backend.service "$HOST_IP" 
 # docker run -i --rm --volumes-from haproxy -t bewt85/configure_haproxy
