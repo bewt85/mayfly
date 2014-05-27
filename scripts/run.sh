6d11b046327ab93ba698834eead6c72a57091ba0
Ben Taylor <bewt85@gmail.com>
2014-05-27 23:09:26 +0000

    Register services on arbitrary ports
    Improve layout of run.sh script to make it easier to start services on
    arbitrary ports

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,25 +5,35 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-HOST_IP=`ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -d: -f2`
+run() {
+  SERVICE=$1
+  VERSION=$2
+  echo "Starting ${SERVICE}.service_${VERSION}" >&2
+  CID=$(docker run -d --name ${SERVICE}_${VERSION} -t -p 8080 --dns $DNS_IP bewt85/${SERVICE}:${VERSION})
+  echo $CID
+}
 
-docker run -d --name dnsmasq   -t                           bewt85/dnsmasq
+register() {
+  SERVICE=$1
+  VERSION=$2
+  PORT=$(docker inspect ${SERVICE}_${VERSION} | ./scripts/extract_docker_port.py 8080)
+  echo "Registering ${SERVICE}.service_${VERSION} on port $PORT" >&2
+  docker run -d -t bewt85/etcd_registrar register ${SERVICE}.service $VERSION $HOST_IP $PORT --peer $HOST_IP:9000
+}
+
+docker run -d --name dnsmasq -t bewt85/dnsmasq
 
 DNS_IP=`docker inspect dnsmasq | awk -F '"' '/IPAddress/ {print $4}'`
+HOST_IP=`ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -d: -f2`
+
+docker run -i --rm --volumes-from dnsmasq bewt85/configure_dns update frontend.service "$HOST_IP" backend.service "$HOST_IP"
 
 docker run -d --name haproxy       -p 80:80                  --dns $DNS_IP bewt85/haproxy
 docker run -d --name etcd-node1 -t -p 7000:7000 -p 9000:9000 --dns $DNS_IP coreos/etcd     -peer-addr ${HOST_IP}:7000 -addr ${HOST_IP}:9000
 
-docker run -d --name backend_0.0.1  -t -p 6000:8080 --dns $DNS_IP bewt85/backend:0.0.1
-docker run -d --name frontend_0.0.1 -t -p 5000:8080 --dns $DNS_IP bewt85/frontend:0.0.1
-docker run -d --name backend_0.0.2  -t -p 6001:8080 --dns $DNS_IP bewt85/backend:0.0.2
-docker run -d --name frontend_0.0.2 -t -p 5001:8080 --dns $DNS_IP bewt85/frontend:0.0.2
+run       backend  0.0.1; register backend  0.0.1
+run       backend  0.0.2; register backend  0.0.2
+run       frontend 0.0.1; register frontend 0.0.1
+run       frontend 0.0.2; register frontend 0.0.2
 
-docker run -d -t bewt85/etcd_registrar register backend.service  0.0.1 $HOST_IP 6000 --peer $HOST_IP:9000
-docker run -d -t bewt85/etcd_registrar register frontend.service 0.0.1 $HOST_IP 5000 --peer $HOST_IP:9000
-docker run -d -t bewt85/etcd_registrar register backend.service  0.0.2 $HOST_IP 6001 --peer $HOST_IP:9000
-docker run -d -t bewt85/etcd_registrar register frontend.service 0.0.2 $HOST_IP 5001 --peer $HOST_IP:9000
-
-docker run -i --rm --volumes-from dnsmasq bewt85/configure_dns update frontend.service "$HOST_IP" backend.service "$HOST_IP" 
-docker run -i --rm --volumes-from dnsmasq bewt85/configure_dns update host1.internal   "$HOST_IP"
 docker run -i --rm --volumes-from haproxy -e "ETCD_PEERS=${HOST_IP}:9000" bewt85/configure_haproxy configure_haproxy.py update
