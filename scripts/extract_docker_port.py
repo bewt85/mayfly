b8dc690ed1e1ddd556266b323993815f18cb01c4
Ben Taylor <bewt85@gmail.com>
2014-05-27 23:09:16 +0000

    Improve documentation of extract_docker_port.py

--- scripts/extract_docker_port.py
+++ scripts/extract_docker_port.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
 import json, sys, argparse
+from argparse import RawTextHelpFormatter
 
-parser = argparse.ArgumentParser()
-parser.add_argument('port')
+description="""Description:
+  A simple script to extract a port from docker inspect
+
+  This script takes an 'internal' docker port and the results of 
+  `docker inspect $CID` and outputs the corresponding port on the host"""
+
+epilog="""Examples:
+  sudo docker inspect <container> | {name} 8080
+  sudo docker inspect <container> | {name} 8080/udp""".format(name=sys.argv[0])
+
+parser = argparse.ArgumentParser(description=description, epilog=epilog, formatter_class=RawTextHelpFormatter)
+parser.add_argument('port', help="Port number within container; defaults to tcp")
 args = parser.parse_args()
 target_port = args.port if '/' in args.port else "%s/tcp" % args.port
 
