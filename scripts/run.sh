ed276f2080fd7ec4d4334d18ad72a9c87449a754
Ben Taylor <bewt85@gmail.com>
2014-07-07 22:14:57 +0000

    Update run script using new container registrar

--- scripts/run.sh
+++ scripts/run.sh
@@ -22,15 +22,7 @@ run() {
   SERVICE=$1
   VERSION=$2
   CID=$(docker run -d -t -p 8080 --dns $DNS_IP ${DOCKER_ACCOUNT_NAME}/${SERVICE}:${VERSION})
-  echo $SERVICE $VERSION $CID
-}
-
-register() {
-  SERVICE=$1
-  VERSION=$2
-  CID=$3
   PORT=$(docker inspect $CID | ./scripts/extract_docker_port.py 8080)
-  CID=$(docker run -d -t ${DOCKER_ACCOUNT_NAME}/container_registrar register ${SERVICE}.service $VERSION $HOST_IP $PORT --peer $HOST_IP:9000)
   echo $SERVICE $VERSION $PORT
 }
 
@@ -39,31 +31,33 @@ CID=$(docker run -d --name dnsmasq -t ${DOCKER_ACCOUNT_NAME}/dnsmasq)
 
 DNS_IP=`docker inspect dnsmasq | awk -F '"' '/IPAddress/ {print $4}'`
 HOST_IP=`ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -d: -f2`
-
-echo "Registering services with DNS"
-CID=$(docker run -i --rm --volumes-from dnsmasq ${DOCKER_ACCOUNT_NAME}/dnsmasq_updater update frontend.service "$HOST_IP" backend.service "$HOST_IP")
-
+ 
 echo "Starting HAProxy"
-CID=$(docker run -d --name haproxy       -p 80:80                  --dns $DNS_IP ${DOCKER_ACCOUNT_NAME}/haproxy)
+CID=$(docker run -d --name haproxy       -p 80:80                  ${DOCKER_ACCOUNT_NAME}/haproxy)
 echo "Starting etcd"
-CID=$(docker run -d --name etcd-node1 -t -p 7000:7000 -p 9000:9000 --dns $DNS_IP coreos/etcd     -peer-addr ${HOST_IP}:7000 -addr ${HOST_IP}:9000)
+CID=$(docker run -d --name etcd-node1 -t -p 7000:7000 -p 9000:9000 coreos/etcd     -peer-addr ${HOST_IP}:7000 -addr ${HOST_IP}:9000)
 
 WAIT=60s
 echo "Giving etcd $WAIT to warm up"
 sleep $WAIT
 
+echo "Registering services with DNS"
+CID=$(docker run -i --rm --volumes-from dnsmasq ${DOCKER_ACCOUNT_NAME}/dnsmasq_updater update frontend.service "$HOST_IP" backend.service "$HOST_IP")
+
 echo "Starting HAProxy config"
-CID=$(docker run -d --volumes-from haproxy --name haproxy_updater -e "ETCD_PEERS=${HOST_IP}:9000" ${DOCKER_ACCOUNT_NAME}/haproxy_updater etcdctl --peers ${HOST_IP}:9000 exec-watch --recursive /mayfly -- bash -c "configure_haproxy.py update")
+CID=$(docker run -d --volumes-from haproxy --name haproxy_updater -e "HOST_IP=${HOST_IP}" -e "ETCD_PEERS=${HOST_IP}:9000" ${DOCKER_ACCOUNT_NAME}/haproxy_updater etcdctl --peers ${HOST_IP}:9000 exec-watch --recursive /mayfly -- bash -c "configure_haproxy.py update")
 echo "Starting HAProxy config updates"
 CID=$(docker run -d -t --name environment_registrar -e 'ETCD_PEERS=10.0.2.15:9000' ${DOCKER_ACCOUNT_NAME}/environment_registrar start_auto_update.sh)
+echo "Starting container registrar"
+CID=$(docker run -d -t -e HOST_IP=$HOST_IP -e ETCD_PEERS=${HOST_IP}:9000 -v /var/run/docker.sock:/var/run/docker.sock ${DOCKER_ACCOUNT_NAME}/container_registrar)
 
-announce $(register $(run backend  0.0.1))
-announce $(register $(run backend  0.0.1))
-announce $(register $(run backend  0.0.1))
-announce $(register $(run frontend 0.0.1))
-announce $(register $(run backend  0.0.2))
-announce $(register $(run backend  0.0.2))
-announce $(register $(run frontend 0.0.2))
+announce $(run backend  0.0.1)
+announce $(run backend  0.0.1)
+announce $(run backend  0.0.1)
+announce $(run frontend 0.0.1)
+announce $(run backend  0.0.2)
+announce $(run backend  0.0.2)
+announce $(run frontend 0.0.2)
 
 echo "Add initial HAProxy config"
 CID=$(cat mayfly-environment-registrar/example_config/prod-example.yaml | sudo docker run --rm -i -t --volumes-from environment_registrar -a stdin ubuntu tee /etc/mayfly/environments/prod.yaml)
